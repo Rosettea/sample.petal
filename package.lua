6d17acd16c09e3caa71ace18a753f2dc18740989
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-04-07 14:14:40 -0400

    feat: add sample files

---
+++ package.lua
@@ -0,0 +1,7 @@
+return {
+	name = 'sample',
+	version = '0.0.1',
+	description = 'Example package for petals',
+	license = 'MIT',
+	dependencies = {}
+}
---
+++ src/plugin.lua
@@ -0,0 +1,5 @@
+local commander = require 'commander'
+
+commander.register('hello', function()
+	print 'Hello world!'
+end)
